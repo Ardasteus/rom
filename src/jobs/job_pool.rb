1ac01573de378f16a89152c8f69308c84c841a62
Roman Španko <romanspankojr@gmail.com>
2019-03-20 10:27:36 +0100

    Fixed Jobs

--- src/jobs/job_pool.rb
+++ src/jobs/job_pool.rb
@@ -17,7 +17,7 @@ module ROM
     end
 
     # Adds a new {ROM::Job} to the job pool, if pool is full, job will be added to a queue
-    # @param [{ROM::Job}] job Job to be added to the pool
+    # @param [ROM::Job] job Job to be added to the pool
     def add_job(job)
       if @capacity != 0 and @running.count == @capacity
         @semaphore.synchronize do
@@ -31,9 +31,9 @@ module ROM
     end
 
     # Called when {ROM::Job} notifies the pool that its state has changed
-    # @param [{ROM::Job}] job Job that raised the event
+    # @param [ROM::Job] job Job that raised the event
     def update_job(job)
-      @running.delete(job)
+      @running.delete(job) unless job.state == :running
       if job.state == :finished and @queue.length > 0
         @semaphore.synchronize do
             add_job(@queue.pop)
@@ -44,7 +44,7 @@ module ROM
     end
 
     # Called when {ROM::Job} failed executing its task
-    # @param [{ROM::Job}] job Job that raised the event
+    # @param [ROM::Job] job Job that raised the event
     def handle_failed(job)
     end
   end
