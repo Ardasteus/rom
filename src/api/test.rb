88ec3f4a5f32389dd522a7b6cc1ce7e617008282
Matyáš Pokorný <matyaspoko@live.com>
2019-03-28 13:27:51 +0100

    Added return types and normalization of signatures
    ROM-29 Implement abstract API system

--- src/api/test.rb
+++ src/api/test.rb
@@ -3,7 +3,7 @@ module ROM
 		class Test < StaticResource
 			namespace :api, :v1
 			
-			action :env do
+			action :env, Types::Hash[String, String], :name => String do
 				return ENV
 			end
 		end
--- src/dynamic/resource.rb
+++ src/dynamic/resource.rb
@@ -33,10 +33,10 @@ module ROM
 			@path = path.collect(&:to_s).join(PATH_SEPARATOR)
 		end
 
-		def self.action(name, *att, **sig, &block)
+		def self.action(name, ret, *att, **sig, &block)
 			raise('Action with same name already defined!') if @act.has_key?(name.to_s)
 			raise('Action name cannot contain path separator!') if name.to_s.include?(PATH_SEPARATOR)
-			act = ResourceAction.new(name.to_s, sig, att, &block)
+			act = ResourceAction.new(name.to_s, ActionSignature.new(ret, sig), att, &block)
 			if att.any? { |i| i.is_a?(DefaultAction) }
 				raise("Default action '#{act.name}' collides with '#{@def.name}'!") unless @def == nil
 				@def = act
@@ -86,6 +86,57 @@ module ROM
 		
 		def attribute?(klass)
 			@att.any? { |i| i.is_a?(klass) }
+		end
+	end
+
+	class ActionSignature
+		def return_type
+			@ret
+		end
+
+		def arguments
+			@sig.keys
+		end
+
+		def initialize(ret, sig)
+			@sig = {}
+			@ret = Types::Type.to_t(ret)
+
+			order = 0
+			sig.each_pair do |k, v|
+				raise("Expecting argument name #{k.inspect} to be a symbol!") unless k.is_a?(Symbol)
+
+				k = k.to_s
+				req = k.end_with?('!')
+				name = (req ? k[0..k.length - 2] : k).to_sym
+				case v
+					when Types::Type
+						@sig[name] = { :type => v, :required => req, :default => nil, :order => order }
+					when Class
+						@sig[name] = { :type => Types::Type.to_t(v), :required => req, :default => nil, :order => order }
+					when Hash
+						raise("Argument '#{name}' doesn't specify type!") unless v.has_key?(:type)
+						v[:required] = req unless v.has_key?(:required)
+						v[:default] = nil unless v.has_key?(:default)
+						v[:order] = order unless v.has_key?(:order)
+						@sig[name] = v
+					else
+						raise("Argument signature for '#{name}' expects to be either a #{Class.name} or #{Hash.name}!")
+				end
+				order += 1
+			end
+		end
+
+		def [](arg)
+			case arg
+				when Symbol
+					return @sig[arg]
+				when Integer
+					@sig.each { |i| return i if i[:order] == arg }
+					return nil
+				else
+					return nil
+			end
 		end
 	end
 	
