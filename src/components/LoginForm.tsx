0aca5b0fb541c334c1dd1e5c1c3773d6aaa664af
Roman Španko <romanspankojr@gmail.com>
2019-05-17 02:12:09 +0200

    Changed LoginForm to Grid

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Input from '@material-ui/core/Input';
-import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 
@@ -8,11 +8,17 @@ class LoginForm extends React.Component {
 	render() {
 		return (
 			<div id="loginform">
-				<Paper elevation={10} >
-					<Input placeholder="Username"></Input>
-					<Input placeholder="Password"></Input>
-					<Button color="primary" onClick={() => {window.open("/", "_self");}}>Login</Button>
-				</Paper>
+				<Grid container spacing={24} color="#e91e63">
+					<Grid item xs={12}>
+						<Input placeholder="Username"></Input>
+					</Grid>
+					<Grid item xs={12}>
+						<Input placeholder="Password"></Input>
+					</Grid>
+					<Grid item xs={12}>
+						<Button color="primary" onClick={() => {window.open("/", "_self");}}>Login</Button>
+					</Grid>
+				</Grid>
 			</div>
 		)
 	}
