8a0ac14357cbafa668ce6a8f38496aded48e67f6
Alexandr Hájek <alexax578@gmail.com>
2019-05-19 00:17:58 +0200

    Scope background to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className='home-page'>
+        <img src={'public/images/homepage-background.jpg'} className='bg' />
         <h1>Grove Street. Home. At least it used to be until I f***ed everything up.</h1>
       </div>
     );
