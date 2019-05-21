7cc016f70e49cc2d95e5384ad86462678ae0bcc3
Nitrikon <n1tr1kon1@gmail.com>
2019-05-21 10:46:16 +0200

    Implemented homepage tabs, settings dialog
    Rom 23 - Create responsive pages in JS

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -16,6 +16,14 @@ import Button from '@material-ui/core/Button';
 import { Redirect } from 'react-router-dom';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import { Transition } from 'react-transition-group';
 
 const styles = createStyles({
   root: {
@@ -28,6 +36,12 @@ const styles = createStyles({
     marginLeft: -12,
     marginRight: 20,
   },
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
 });
 
 export interface Props extends WithStyles<typeof styles> {}
@@ -36,6 +50,8 @@ export interface State {
   redirect: any;
   auth: boolean;
   anchorEl: null | HTMLElement;
+  value: boolean;
+  open: boolean;
 }
 
 class NavBar extends React.Component<Props, State> {
@@ -43,7 +59,16 @@ class NavBar extends React.Component<Props, State> {
     auth: true,
     anchorEl: null,
     redirect: false,
+    value: false,
+    open: false
   };
+  TabContainer = (props) => {
+    return (
+      <Typography component="div" style={{ padding: 8 * 3 }}>
+        {props.children}
+      </Typography>
+    );
+  }
   setRedirect = () => {
     this.setState({
       redirect: true,
@@ -58,6 +83,9 @@ class NavBar extends React.Component<Props, State> {
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ auth: event.target.checked });
   }
+  handleChange1 = (event, value) => {
+    this.setState({ value });
+  }
 
   handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     this.setState({ anchorEl: event.currentTarget });
@@ -66,20 +94,78 @@ class NavBar extends React.Component<Props, State> {
   handleClose = () => {
     this.setState({ anchorEl: null });
   }
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+  
+  handleClose1 = () => {
+    this.setState({ open: false });
+  };
 
   render() {
     const { classes } = this.props;
     const { auth, anchorEl } = this.state;
     const open = Boolean(anchorEl);
+    const { value } = this.state;
 
     return (
       <div className={classes.root}>
         <AppBar position='static'>
+        <Dialog
+          fullScreen
+          open={this.state.open}
+          onClose={this.handleClose1}
+          TransitionComponent={Transition}
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton color="inherit" onClick={this.handleClose1} aria-label="Close">
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={classes.flex}>
+                Settings
+              </Typography>
+              <Button color="inherit" onClick={this.handleClose1}>
+                save
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <List>
+            <ListItem button>
+              <ListItemText primary="Dashboard"/>
+            </ListItem>
+            
+            <ListItem button>
+              <ListItemText primary="Personal data"/>
+            </ListItem>
+            
+            <ListItem button>
+              <ListItemText primary="Data and personilaziton"/>
+            </ListItem>
+            
+            <ListItem button>
+              <ListItemText primary="Security"/>
+            </ListItem>
+            
+            <ListItem button>
+              <ListItemText primary="People and sharing"/>
+            </ListItem>
+            <Divider />
+            <ListItem button>
+              <ListItemText primary="Send feedback"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Report bugs"/>
+            </ListItem>
+          </List>
+        </Dialog>
           <Toolbar>
             <Typography variant='h6' color='inherit' className={classes.grow}>
               Ruby On Mails
-              <Button color='inherit'>Inbox</Button>
-              <Button color='inherit'>Chat</Button>
+              <Tabs value={value} onChange={this.handleChange1}>
+               <Tab label="Inbox" />
+               <Tab label="Chat" />
+              </Tabs>
             </Typography>
             {auth && (
               <div>
@@ -107,13 +193,15 @@ class NavBar extends React.Component<Props, State> {
                 >
                   {this.loginRedirect()}
                   <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>Settings</MenuItem>
+                  <MenuItem onClick={this.handleClickOpen}>Settings</MenuItem>
                   <MenuItem onClick={this.setRedirect}>Logout</MenuItem>
                 </Menu>
               </div>
             )}
           </Toolbar>
         </AppBar>
+        {value == true && <this.TabContainer>Item One</this.TabContainer>}
+        {value != false && <this.TabContainer>Item Two</this.TabContainer>}
       </div>
     );
   }
