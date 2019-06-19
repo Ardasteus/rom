95530beef87565e436bd959b1dd6a006b5ddf493
Roman Španko <romanspankojr@gmail.com>
2019-06-19 22:07:24 +0200

    Initial React

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import RouterComponent from 'components/RouterComponent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <RouterComponent />
+    );
+  }
+}
+
+export default App;
